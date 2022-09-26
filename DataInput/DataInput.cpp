f6d08748b665fbab269cc495f04f9530e0bee86b
Aaron McDonald <sergeantbeast456@gmail.com>
2022-09-26 13:25:13 -0400

    Initial Commit

---
+++ DataInput/DataInput.cpp
@@ -0,0 +1,6 @@
+// DataInput.cpp 
+// By: Aaron McDonald
+// This library contains functions for reading in data from the user using the console
+
+#include "pch.h"
+#include "framework.h"
---
+++ DataInput/DataInput.h
@@ -0,0 +1,46 @@
+// Data Input
+// By: Aaron McDonald
+// Function for reading in a double value from the console
+
+#include <string>
+#include <iostream>
+
+// Gets a double value from the user while ensuring that the data is valid
+double get_double() {
+	std::string valid_characters = "0123456789."; // Declaring all of the valid characters that are acceptable input
+	std::string tester_string; // used for validating data
+	bool has_decimal; // used for ensuring there is only one decimal 
+	bool all_num;
+	while (true) {
+		std::getline(std::cin, tester_string);
+		all_num = true;
+		has_decimal = false;
+		// Looping over the string and testing to ensure that each charater is a part of the valid characters set
+		for (int i = 0; i < tester_string.size(); i++) {
+			if (valid_characters.find(tester_string[i]) == std::string::npos) {
+				std::cout << "Please enter only numbers or a period, no characters: ";
+				all_num = false;
+				break;
+			}
+
+			// Ensuring that the string only contains one decimal point
+			else if (tester_string[i] == '.') {
+				if (has_decimal) {
+					all_num = false;
+					std::cout << "Please only enter one decimal point: ";
+					break;
+				}
+				has_decimal = true;
+			}
+		}
+
+		// Returning the string made double assuming all requirements are met
+		if (all_num) {
+			return stod(tester_string);
+		}
+		else {
+			continue;
+		}
+
+	}
+}
